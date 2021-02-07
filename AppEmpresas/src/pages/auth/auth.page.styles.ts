2bc1920d949bb20a10db93e548aef191132bb773
dkotsuka <dkotsuka@gmail.com>
2021-02-07 20:51:08 -0300

    add authentication messages

--- AppEmpresas/src/pages/auth/auth.page.styles.ts
+++ AppEmpresas/src/pages/auth/auth.page.styles.ts
@@ -5,3 +5,10 @@ export const AuthPageContainer = styled.View`
     justify-content: center;
 `
 
+export const LogoutMessage = styled.Text`
+    color: saddlebrown;
+    font-size: 16px;
+    margin-horizontal: 16px;
+    margin-bottom: 4px;
+    text-align: center
+`
